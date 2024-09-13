862f23fe62274c7167458d3576ef3aa9e014e5c6
SplitMilky <81430176+SplitMilky@users.noreply.github.com>
2024-09-13 13:23:56 -0300

    Switched to reflection based method (which is probably slower)

--- CameraZoom/CameraPatches.cs
+++ CameraZoom/CameraPatches.cs
@@ -1,135 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
+﻿using System.Collections.Generic;
 using HarmonyLib;
 using Sandbox.Engine.Utils;
 using VRageMath;
+using System.Reflection;
 
 namespace Camera.Zoom
 {
-	[HarmonyPatch]
-	public class CameraPatches
-	{
-		static CameraPatches()
-		{
-			characterSpring = AccessTools.Field(typeof(MyThirdPersonSpectator), "NormalSpringCharacter");
-			normalSpring = AccessTools.Field(typeof(MyThirdPersonSpectator), "NormalSpring");
-			lookAt = AccessTools.Field(typeof(MyThirdPersonSpectator), "m_lookAt");
-			clampedlookAt = AccessTools.Field(typeof(MyThirdPersonSpectator), "m_clampedlookAt");
-		}
+    [HarmonyPatch]
+    public class CameraPatches
+    {
+        static CameraPatches()
+        {
+            characterSpring = AccessTools.Field(typeof(MyThirdPersonSpectator), "NormalSpringCharacter");
+            normalSpring = AccessTools.Field(typeof(MyThirdPersonSpectator), "NormalSpring");
+            lookAt = AccessTools.Field(typeof(MyThirdPersonSpectator), "m_lookAt");
+            clampedlookAt = AccessTools.Field(typeof(MyThirdPersonSpectator), "m_clampedlookAt");
+        }
 
-		public static BoundingBox Fix(BoundingBox bb)
-		{
-			bb.Scale(new Vector3(0.0001f));
-			return bb;
-		}
+        private static FieldInfo characterSpring;
+        private static FieldInfo normalSpring;
+        private static FieldInfo lookAt;
+        private static FieldInfo clampedlookAt;
 
-		public static BoundingBoxD Fix2(BoundingBoxD worldAABB)
-		{
-			return new BoundingBoxD(worldAABB.Center - new Vector3D(0.0001), worldAABB.Center + new Vector3D(0.0001));
-		}
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MyThirdPersonSpectator), "Update")]
+        public static void UpdatePrefix(MyThirdPersonSpectator __instance)
+        {
+		    if (__instance == null)
+            {
+                return; // Early exit if __instance is null
+            }
 
-		[HarmonyTranspiler]
-		[HarmonyPatch(typeof(MyThirdPersonSpectator), "ComputeEntitySafeOBB")]
-		public static IEnumerable<CodeInstruction> ComputeEntitySafeOBBTranspiler(IEnumerable<CodeInstruction> instructions)
-		{
-			foreach (CodeInstruction code in instructions)
-			{
-				if (code.opcode == OpCodes.Stloc_1)
-				{
-					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(CameraPatches), "Fix", null, null));
-					yield return code;
-				}
-				else if (code.LoadsField(AccessTools.Field(typeof(MyThirdPersonSpectator), "m_safeAABB")))
-				{
-					yield return code;
-					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(CameraPatches), "Fix2", null, null));
-				}
-				else
-					yield return code;
-			}
-		}
+            // Custom logic to prevent camera zoom-in
+            // For example, you can adjust the camera position or ignore certain collisions
+            PropertyInfo cameraPositionProperty = __instance.GetType().GetProperty("CameraPosition", BindingFlags.NonPublic | BindingFlags.Instance);
+            PropertyInfo targetPositionProperty = __instance.GetType().GetProperty("TargetPosition", BindingFlags.NonPublic | BindingFlags.Instance);
 
+            if (cameraPositionProperty == null || targetPositionProperty == null)
+            {
+                return; // Early exit if properties are not found
+            }
 
-		[HarmonyPrefix]
-		[HarmonyPatch(typeof(MyThirdPersonSpectator), "IsCameraForced")]
-		public static bool IsCameraForced(ref bool __result)
-		{
-			__result = false;
-			return false;
-		}
+            Vector3D cameraPosition = cameraPositionProperty.GetValue(__instance) as Vector3D? ?? Vector3D.Zero;
+            Vector3D targetPosition = targetPositionProperty.GetValue(__instance) as Vector3D? ?? Vector3D.Zero;
 
-		[HarmonyPrefix]
-		[HarmonyPatch(typeof(MyThirdPersonSpectator), "IsEntityFiltered")]
-		public static bool IsEntityFiltered(ref bool __result)
-		{
-			__result = true;
-			return false;
-		}
+            // Example: Prevent camera from zooming in if the distance to the target is less than a threshold
+            double minDistance = 5.0; // Adjust this value as needed
+            if (Vector3D.Distance(cameraPosition, targetPosition) < minDistance)
+            {
+                // Adjust the camera position to maintain the minimum distance
+                Vector3D direction = Vector3D.Normalize(cameraPosition - targetPosition);
+                cameraPositionProperty.SetValue(__instance, targetPosition + direction * minDistance);
+            }
+        }
 
-		[HarmonyPrefix]
-		[HarmonyPatch(typeof(MyThirdPersonSpectator), "MergeAABB")]
-		public static bool MergeAABB()
-		{
-			return false;
-		}
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MyThirdPersonSpectator), "IsCameraForced")]
+        public static bool IsCameraForced(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
 
-		[HarmonyTranspiler]
-		[HarmonyPatch(typeof(MyThirdPersonSpectator), "RaycastOccludingObjects")]
-		public static IEnumerable<CodeInstruction> RaycastOccludingObjectsTranspiler(IEnumerable<CodeInstruction> instructions)
-		{
-			yield return new CodeInstruction(OpCodes.Ldc_I4_0, null);
-			yield return new CodeInstruction(OpCodes.Ret, null);
-			yield break;
-		}
-
-		[HarmonyTranspiler]
-		[HarmonyPatch(typeof(MyThirdPersonSpectator), "RecalibrateCameraPosition")]
-		public static IEnumerable<CodeInstruction> RecalibrateCameraPositionTranspiler(IEnumerable<CodeInstruction> instructions)
-		{
-			foreach (CodeInstruction codeInstruction in instructions)
-			{
-				if (codeInstruction.Calls(AccessTools.Method("VRageMath.BoundingBox:Inflate", new Type[]
-				{
-					typeof(float)
-				}, null)))
-				{
-					yield return new CodeInstruction(OpCodes.Pop, null);
-					yield return new CodeInstruction(OpCodes.Pop, null);
-				}
-				else
-				{
-					yield return codeInstruction;
-				}
-			}
-		}
-
-		[HarmonyTranspiler]
-		[HarmonyPatch(typeof(MyThirdPersonSpectator), "Update")]
-		public static IEnumerable<CodeInstruction> UpdateTranspiler(IEnumerable<CodeInstruction> instructions)
-		{
-			foreach (CodeInstruction codeInstruction in instructions)
-			{
-				if (codeInstruction.LoadsField(normalSpring, false))
-				{
-					yield return new CodeInstruction(OpCodes.Ldfld, characterSpring);
-				}
-				else if (codeInstruction.LoadsField(clampedlookAt, false))
-				{
-					yield return new CodeInstruction(OpCodes.Ldfld, lookAt);
-				}
-				else
-				{
-					yield return codeInstruction;
-				}
-			}
-		}
-
-		private static FieldInfo clampedlookAt;
-		private static FieldInfo lookAt;
-		private static FieldInfo characterSpring;
-		private static FieldInfo normalSpring;
-	}
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MyThirdPersonSpectator), "IsEntityFiltered")]
+        public static bool IsEntityFiltered(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
 }
